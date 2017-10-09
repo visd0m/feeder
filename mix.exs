bf92199dc5eeddd38852d216252d8c5906701d47
Domenico Visconti <domenico.visconti819@gmail.com>
2017-10-09 23:46:19 +0200

    increase release number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FeederBot.Mixfile do
   def project do
     [
       app: :feeder_bot,
-      version: "1.0.4",
+      version: "1.0.5",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
